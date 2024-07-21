88730263c82d1e521f531471542076f87a78d431
Mihail Gumennii <mihail.gumennii@carvana.com>
2024-07-21 16:44:26 -0500

    feat: include playfair display font

---
+++ src/fonts/index.ts
@@ -0,0 +1,7 @@
+import { Playfair_Display } from 'next/font/google';
+
+export const playfairDisplay = Playfair_Display({
+  subsets: ['latin'],
+  variable: '--font-serif',
+  display: 'swap',
+});

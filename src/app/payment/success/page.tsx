3305d9ede81722dc6b72bbebfc9766437713baa9
Mihail Gumennii <mihail.gumennii@carvana.com>
2024-07-21 17:44:12 -0500

    feat: payment success page

---
+++ src/app/payment/success/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className='mx-auto mt-40 max-w-sm px-2 text-center sm:px-0'>
+      <h1 className='font-serif text-3xl font-bold text-primary-foreground'>
+        Thank you for your payment!
+      </h1>
+    </div>
+  );
+}

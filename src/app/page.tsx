b0a9a1d4a18d3b9bb5e445b2b72d0a912b34ec3d
Mihail Gumennii <mihail.gumennii@carvana.com>
2024-07-22 10:55:44 -0500

    feat: add payment pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className='relative min-h-screen h-screen w-full bordex-box pt-16'>
+    <main className='bordex-box h-screen min-h-screen w-full pt-16'>
       <div className='flex h-1/2 w-full items-center justify-center'>
         <div className='mx-auto flex max-w-sm flex-col items-center gap-y-4 px-2 sm:px-0'>
           <h1 className='font-serif text-3xl font-bold text-primary-foreground'>
@@ -14,7 +14,7 @@ export default function Home() {
           </p>
         </div>
       </div>
-      <div className='absolute bottom-0 left-0 h-1/2 w-screen rounded-t-[32px] bg-white p-8'>
+      <div className='h-1/2 w-screen rounded-t-[32px] bg-white p-8'>
         <div className='mx-auto max-w-sm'>
           <div className='mb-6 flex items-center justify-between'>
             <div className='text-base font-bold tracking-[0.2px] text-muted'>
--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -7,6 +7,7 @@ import { Accordion, AccordionItem } from '@/components/shared';
 export default function Page() {
   const [activeId, setActiveId] = useState<string | null>('paymentInformation');
   const [filledForm, setFilledForm] = useState(false);
+  const [userCard, setUserCard] = useState('');
 
   return (
     <div className='mx-auto max-w-xl bg-white pt-20 md:mt-24 md:rounded-xl md:p-6'>
@@ -16,20 +17,23 @@ export default function Page() {
           id='paymentInformation'
           isOpen={activeId === 'paymentInformation'}
           number='1'
-          isFilled={filledForm}
+          showEdit={filledForm}
           onClick={() => setActiveId('paymentInformation')}
         >
-          <PaymentForm setActiveId={setActiveId} setFilled={setFilledForm} />
+          <PaymentForm
+            setActiveId={setActiveId}
+            setFilled={setFilledForm}
+            setUserCard={setUserCard}
+          />
         </AccordionItem>
         <AccordionItem
           title='Review and pay'
           id='review'
           isOpen={activeId === 'review'}
           number='2'
-          isDisable={activeId !== 'review'}
           className='border-none'
         >
-          <Review />
+          <Review cardNumber={userCard} />
         </AccordionItem>
       </Accordion>
     </div>

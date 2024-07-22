aa42e3b27c8990e1873379d78b520ae6a8684d2a
Mihail Gumennii <mihail.gumennii@carvana.com>
2024-07-22 11:01:18 -0500

    feat: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: 'var(--background)',
+        button: {
+          DEFAULT: 'var(--button)',
+          foreground: 'var(--button-foreground)',
+        },
+        error: 'var(--error)',
+        input: 'var(--input)',
+        primary: {
+          DEFAULT: 'var(--primary)',
+          foreground: 'var(--primary-foreground)',
+        },
+        muted: {
+          DEFAULT: 'var(--muted)',
+          foreground: 'var(--muted-foreground)',
+        },
       },
+    },
+    fontFamily: {
+      sans: ['Arial', 'Helvetica', 'sans-serif'],
+      serif: ['var(--font-serif)', '"Times New Roman", serif'],
+    },
+    fontSize: {
+      '3xl': '1.75rem',
+    },
+    maxWidth: {
+      sm: '376px',
+      xl: '576px',
     },
   },
   plugins: [],
